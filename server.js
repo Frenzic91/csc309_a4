5b1e35b0519283626771ab6ee58ca13ddf6251ac
Sandi <s.milicevic@mail.utoronto.ca>
2015-11-08 19:40:52 -0500

    Added signup form

--- server.js
+++ server.js
@@ -10,5 +10,9 @@ app.get('/home', function(req, res) {
 	res.sendFile(path.join(__dirname + '/views/home.html'));
 });
 
+app.get('/signup', function(req, res) {
+	res.sendFile(path.join(__dirname + '/views/signup.html'));
+});
+
 app.listen(3000);
 console.log('Server listening on port 3000...');
